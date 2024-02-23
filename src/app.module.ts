ec0bf54fdfbcf56fe8eb7425670347b7718de0ba
postfm <72159645+postfm@users.noreply.github.com>
2024-02-23 18:56:14 +0300

    Добавит модули

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CategoriesModule } from './categories/categories.module';
+import { AuthModule } from './auth/auth.module';
+import { TransactionsModule } from './transactions/transactions.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, CategoriesModule, AuthModule, TransactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
